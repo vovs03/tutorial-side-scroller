245a36617bce86c69562d0c5d98375133d876b80
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2024-09-20 04:42:49 +0300

    Add: Bonus star 🌟 include

---
+++ stars/bonus_star.go
@@ -0,0 +1,44 @@
+components {
+  id: "bonus_star"
+  component: "/stars/bonus_star.script"
+}
+components {
+  id: "pickup2"
+  component: "/stars/pickup.particlefx"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"bonus_star\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/stars/stars.atlas\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: -2.0\n"
+  "      y: -1.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 27.5\n"
+  "}\n"
+  ""
+}
